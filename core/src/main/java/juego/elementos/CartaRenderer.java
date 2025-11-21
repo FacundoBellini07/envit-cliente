386bcfbf11080e74900646a5cb820c8229d08670
Francisco Colombo <frnclmb@gmail.com>
2025-11-21 14:56:08 -0300

    Se aplican bordes redondeados a las cartas

--- core/src/main/java/juego/elementos/CartaRenderer.java
+++ core/src/main/java/juego/elementos/CartaRenderer.java
@@ -24,13 +24,22 @@ public class CartaRenderer {
         carta.updateLimites(x, y, width, height);
         Rectangle limites = carta.getLimites();
 
+        float r = 6f;
+
         shapeRenderer.setProjectionMatrix(viewport.getCamera().combined);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(backgroundColor);
 
-        shapeRenderer.rect(limites.x, limites.y, limites.width, limites.height);
+        shapeRenderer.rect(limites.x + r, limites.y, limites.width - 2 * r, limites.height);
+        shapeRenderer.rect(limites.x, limites.y + r, limites.width, limites.height - 2 * r);
+
+        shapeRenderer.arc(limites.x + r, limites.y + r, r, 180, 90);
+        shapeRenderer.arc(limites.x + limites.width - r, limites.y + r, r, 270, 90);
+        shapeRenderer.arc(limites.x + r, limites.y + limites.height - r, r, 90, 90);
+        shapeRenderer.arc(limites.x + limites.width - r, limites.y + limites.height - r, r, 0, 90);
 
         shapeRenderer.end();
+
         batch.begin();
         carta.draw(batch, x, y, width, height);
         batch.end();
